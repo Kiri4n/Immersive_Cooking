33ddad8fd736297711da1230b22cc2efe5da47ca
Kiri4n <kirian.jourand@gmail.com>
2021-03-25 07:03:47 +0100

    + block: cutting board + item: knife_tall

--- src/main/java/fr/luki/immersivecooking/init/ModItems.java
+++ src/main/java/fr/luki/immersivecooking/init/ModItems.java
@@ -1,16 +1,26 @@
 package fr.luki.immersivecooking.init;
 
 import fr.luki.immersivecooking.immcook;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
-    public static final Item testItem = new Item(new Item.Settings().group(ItemGroup.MISC));
-    public static final Item knife = new Item(new Item.Settings().group(ItemGroup.MISC));
-
     public static void registerAll(){
-        Registry.register(Registry.ITEM,new Identifier(immcook.MODID, "knife"), knife);
+        //Items
+        Registry.register(Registry.ITEM,new Identifier(immcook.MODID, "knife"), KNIFE);
+        Registry.register(Registry.ITEM,new Identifier(immcook.MODID, "knife_tall"), KNIFE_TALL);
+
+        //Block items
+        Registry.register(Registry.ITEM, new Identifier(immcook.MODID, "cutting_board"), CUTTING_BOARD);
     }
+
+    //Items
+    public static final Item KNIFE = new Item(new Item.Settings().group(immcook.ITEM_GROUP));
+    public static final Item KNIFE_TALL = new Item(new Item.Settings().group(immcook.ITEM_GROUP));
+
+    //Block items
+    public static final BlockItem CUTTING_BOARD = new BlockItem(ModBlocks.CUTTING_BOARD, new Item.Settings().group(immcook.ITEM_GROUP));
 }
